cd17cce416276c2e0c3d7decf6058b568201c728
Alexander <82713915+EtoZheSandy@users.noreply.github.com>
2024-03-31 21:26:37 +0300

    init

--- app/src/main/java/su/afk/coursecurrencypsbank/CurrencyAdapter.kt
+++ app/src/main/java/su/afk/coursecurrencypsbank/CurrencyAdapter.kt
@@ -1,6 +1,7 @@
 package su.afk.coursecurrencypsbank
 
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import su.afk.coursecurrencypsbank.databinding.CurrencyItemBinding
@@ -17,22 +18,16 @@ class CurrencyAdapter(private var currencies: List<Currency>) :
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CurrencyViewHolder {
         val inflater: LayoutInflater = LayoutInflater.from(parent.context)
         val binding = CurrencyItemBinding.inflate(inflater, parent, false)
-        return CurrencyViewHolder(binding)
+        return CurrencyViewHolder(binding.root)
     }
 
     /*
     * Метод onBindViewHolder() вызывается для привязки данных к представлению элемента списка в указанной позиции.
-    * Здесь мы получаем объект Currency из списка currencies по заданной позиции и передаем его в
+    * Здесь мы передаем объект Currency из списка currencies по заданной позиции и передаем его в
     * метод bind() класса CurrencyViewHolder, который обновляет данные элемента списка.
     * */
-    override fun onBindViewHolder(holder: CurrencyAdapter.CurrencyViewHolder, position: Int) {
-        val currency = currencies[position]
-
-        with(holder.binding) {
-            tvCharCode.text = currency.CharCode
-            tvName.text = currency.Name
-            tvValue.text = currency.Value.toString()
-        }
+    override fun onBindViewHolder(holder: CurrencyViewHolder, position: Int) {
+        holder.bind(currencies[position])
     }
 
     /*
@@ -44,10 +39,16 @@ class CurrencyAdapter(private var currencies: List<Currency>) :
     }
 
     /*
-    * Наследуем CurrencyViewHolder от RecyclerView.ViewHolder
-    * Передаем ему в констуктор наш binding
-    * */
-    class CurrencyViewHolder(val binding: CurrencyItemBinding) : RecyclerView.ViewHolder(binding.root) {
+    * Заполняет данными каждый переданный view
+     */
+    class CurrencyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        private val binding = CurrencyItemBinding.bind(itemView)
+
+        fun bind(currency: Currency) =with(binding) {
+            tvCharCode.text = currency.CharCode
+            tvName.text = currency.Name
+            tvValue.text = currency.Value.toString()
+        }
     }
 
     /*  Функция для обновления списка данных
